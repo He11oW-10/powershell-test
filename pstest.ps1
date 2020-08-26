b8696c22b75d02ec01b22fd99fe6dd41400b2753
Yili Zhou <yilizh@wicresoft.com>
2020-08-26 10:45:35 +0800

    first commit

---
+++ pstest.ps1
@@ -0,0 +1,85 @@
+﻿
+#update virtual directories and applications with powershell
+
+#1.generate json file
+#(1)echo
+echo -n '{
+   "virtualApplications":[
+       {
+           "physicalPath":"site\\wwwroot",
+           "preloadEnabled":"true",
+           "virtualDirectories":[
+              {
+                 "physicalPath":"site\\wwwroot\\6776",
+                 "virtualPath":"/6776"
+              }
+           ],
+           "virtualPath":"/"
+       },
+       {  
+           "physicalPath":"site\\12345",
+           "preloadEnabled":"false",
+           "virtualPath":"/12345"
+       }
+   ]
+}' > 1234.json
+
+
+#(2)hastables
+$var4=@{
+   virtualApplications=@(
+   @{
+      physicalPath="site\wwwroot"
+      preloadEnabled=$true
+      virtualPath="/"
+      virtualDirectories=@(
+         @{
+         physicalPath= "site\wwwroot\6776"
+         virtualPath="/6776"
+         }
+      )
+    },   
+   @{
+       physicalPath="site\int98"
+       preloadEnabled=$false
+       virtualPath="/int98"
+    }
+)
+}
+
+
+$result3=$var4 | ConvertTo-Json -Depth 5
+$result3 >5678.json
+
+
+#2. update
+az resource update --name <web-app-name> --resource-group <resource-group-name> --resource-type "Microsoft.Web/sites" --set properties.siteConfig=@5678.json --debug
+#or
+az webapp config set -g <resource-group-name> -n <web-app-name> --generic-configurations '@5678.json' --verbose
+
+
+
+
+#3. user another api.
+echo -n '[
+       {
+           "physicalPath":"site\\wwwroot",
+           "preloadEnabled":"true",
+           "virtualDirectories":[
+              {
+                 "physicalPath":"site\\wwwroot\\6776",
+                 "virtualPath":"/6776"
+              }
+           ],
+           "virtualPath":"/"
+       },
+       {  
+           "physicalPath":"site\\12345",
+           "preloadEnabled":"false",
+           "virtualPath":"/12345"
+       }
+   ]' > 91011.json
+
+
+az resource update --name "<web-app-name>/config/web" --resource-group <resource-group-name> --resource-type "Microsoft.Web/sites/config" --set properties.virtualApplications=$91011.json --debug
+
---
+++ pstest2.ps1
@@ -0,0 +1,25 @@
+﻿#update allowedAudience with powershell
+
+$PropertiesObject=@{
+   "enabled"="true";
+   "allowedAudience"="https://a.awtst.com", "https://b.awtst.com";
+   "clientId"="<client-id>"
+}
+New-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName <resource-group-name> -ResourceType Microsoft.Web/sites/config -ResourceName "<web-app-name>/authsettings" -ApiVersion 2018-02-01 -Force -Debug
+
+
+#for one web app in allowedAudience
+
+$test="https://c.awtst.com"
+$ary=[string[]]$test
+$ary.GetType()
+
+$PropertiesObject2=@{
+   allowedAudience=$ary
+}
+
+
+#https://docs.microsoft.com/en-us/cli/azure/webapp/auth?view=azure-cli-latest#az-webapp-auth-update
+
+#arm template : use api version 2015-08-01
+#https://github.com/Azure-Samples/ContosoInsurance/blob/928d991742898fb9daabc91b9328749a9dc90026/azuredeploy.json#L1054-L1086
